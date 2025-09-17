ce9de091f45e4baf70fb41dae80ef87fe2e86dc9
Tosin <tosinmath007@gmail.com>
2025-09-17 19:05:47 +0100

    App version update to 1.2.1 (22)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -197,8 +197,8 @@ android {
         applicationId = "com.module.notelycompose.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 21
-        versionName = "1.2.0"
+        versionCode = 22
+        versionName = "1.2.1"
     }
     buildFeatures {
         compose = true
